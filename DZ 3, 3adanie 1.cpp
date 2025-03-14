e4b185621ae03d16b1f1207ead7201d90cd98900
wyr0o <brykin-2006@mail.ru>
2025-03-14 15:35:02 +0300

    Add files via upload

---
+++ DZ 3, 3adanie 1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	bool x = true;
+	bool y = true;
+
+	cout << "оператор: || "<< endl;
+
+	cout << boolalpha<< x<< " " << y<< "  | " << x||y;
+	cout <<endl<< boolalpha << x << " " << !y << " | " << (x || (!y));
+	cout << endl << boolalpha << !x << " " << y << " | " << ((!x) || y);
+	cout << endl << boolalpha << !x << " " << !y << "| " << ((!x) || (!y));
+
+
+	cout <<endl<< endl<< "оператор: && " << endl;
+
+	cout << boolalpha << x << " " << y << "  | " << x && y;
+	cout <<endl<< boolalpha << x << " " << !y << " | " << (x && (!y));
+	cout << endl << boolalpha << !x << " " << y << " | " << ((!x) && y);
+	cout << endl << boolalpha << !x << " " << !y << "| " << ((!x) && (!y));
+
+	cout << endl;
+
+}
